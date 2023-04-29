bc3af628c5507130d54dc4e743b668fd1c33f4d1
Jakob Beckmann <jakobbeckmann@pm.me>
2023-04-29 11:43:58 +0200

    feat: reduced padding in wezterm windows

--- home/wezterm/default.nix
+++ home/wezterm/default.nix
@@ -22,8 +22,12 @@
         { key = 'UpArrow', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
         { key = 'DownArrow', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
       }
-
-      config.harfbuzz_features = { "dlig" }
+      config.window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0
+      }
 
       return config
     '';
