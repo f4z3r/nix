452e45d90ebfdc352728f96b0b06903544afec1a
Jakob Beckmann <jakobbeckmann@pm.me>
2023-05-22 22:11:28 +0200

    feat: add quickfix and locationlist bindings

---
+++ home/apps/nvim/lua/bindings/locationlist.lua
@@ -0,0 +1,35 @@
+-- Locationlist bindings
+
+local leader = '<leader>l'
+
+local mappings = {
+  {
+    mode = 'n',
+    suffix = 'l',
+    command = function() require('telescope.builtin').loclist() end,
+    desc = 'Search location list',
+  },
+  {
+    mode = 'n',
+    suffix = 'o',
+    command = '<cmd>lopen<cr>',
+    desc = 'Open location list',
+  },
+  {
+    mode = 'n',
+    suffix = 'c',
+    command = '<cmd>lclose<cr>',
+    desc = 'Close location list',
+  },
+}
+
+vim.keymap.set('n', ']l', '<cmd>lnext<cr>', {
+  desc = 'Move to next entry in location list',
+})
+vim.keymap.set('n', '[l', '<cmd>lprevious<cr>', {
+  desc = 'Move to previous entry in location list',
+})
+
+for _, mapping in ipairs(mappings) do
+  vim.keymap.set(mapping.mode, leader .. mapping.suffix, mapping.command, { desc = mapping.desc })
+end
---
+++ home/apps/nvim/lua/bindings/quicklist.lua
@@ -0,0 +1,35 @@
+-- Quicklist bindings
+
+local leader = '<leader>q'
+
+local mappings = {
+  {
+    mode = 'n',
+    suffix = 'q',
+    command = function() require('telescope.builtin').quickfix() end,
+    desc = 'Search quickfix list',
+  },
+  {
+    mode = 'n',
+    suffix = 'o',
+    command = '<cmd>copen<cr>',
+    desc = 'Open quickfix list',
+  },
+  {
+    mode = 'n',
+    suffix = 'c',
+    command = '<cmd>cclose<cr>',
+    desc = 'Close quickfix list',
+  },
+}
+
+vim.keymap.set('n', ']q', '<cmd>cnext<cr>', {
+  desc = 'Move to next entry in quickfix list',
+})
+vim.keymap.set('n', '[q', '<cmd>cprevious<cr>', {
+  desc = 'Move to previous entry in quickfix list',
+})
+
+for _, mapping in ipairs(mappings) do
+  vim.keymap.set(mapping.mode, leader .. mapping.suffix, mapping.command, { desc = mapping.desc })
+end
