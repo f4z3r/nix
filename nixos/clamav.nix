0f3ffc1ed91923b80d5257a31621369d32c79ff3
Jakob Beckmann <jakobbeckmann@pm.me>
2023-06-05 18:17:54 +0200

    fix(clamav): remove forced stop of clamonacc service

--- nixos/clamav.nix
+++ nixos/clamav.nix
@@ -39,7 +39,6 @@
       ExecStartPre = ''${pkgs.bash}/bin/bash -c "while [ ! -S /run/clamav/clamd.ctl ]; do sleep 1; done"'';
       ExecStart = ''${pkgs.clamav}/bin/clamonacc -F -c /etc/clamav/clamd.conf --move /root/quarantine  --fdpass --allmatch'';
       ExecReload = ''${pkgs.coreutils}/bin/kill -USR2 $MAINPID'';
-      ExecStop = ''${pkgs.coreutils}/bin/kill -9 $MAINPID'';
     };
   };
 }
