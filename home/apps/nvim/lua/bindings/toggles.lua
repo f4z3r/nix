2be4f1d31e17b7c6b8f541383c58871a9857350e
Jakob Beckmann <jakobbeckmann@pm.me>
2023-06-10 20:49:01 +0200

    fix(nvim): update hint on toggle for maximization

--- home/apps/nvim/lua/bindings/toggles.lua
+++ home/apps/nvim/lua/bindings/toggles.lua
@@ -44,7 +44,7 @@ local mappings = {
     mode = 'n',
     suffix = 'm',
     command = function() require('maximize').toggle() end,
-    desc = 'Toggle workspace diagnostics',
+    desc = 'Toggle maximization',
   },
   {
     mode = 'n',
