2a6388c7ab33a6da8273ef6f53b4cc9c2d1239f8
Jakob Beckmann <jakobbeckmann@pm.me>
2023-08-04 22:07:35 +0200

    fix(orgmode): update bullets unicode

--- home/apps/nvim/plugin/org-bullets.lua
+++ home/apps/nvim/plugin/org-bullets.lua
@@ -1,6 +1,7 @@
 require("org-bullets").setup {
   concealcursor = false,
   symbols = {
+    headlines = { "◉", "○", "⊛", "" },
     checkboxes = {
       half = { "", "OrgTSCheckboxHalfChecked" },
       done = { "", "OrgDone" },
