3d2283e43a75ccac2a774877bbd399dd67889458
Jakob Beckmann <jakobbeckmann@pm.me>
2023-10-30 08:03:10 +0100

    feat: add delayed state to orgmode

--- home/apps/nvim/plugin/orgmode.lua
+++ home/apps/nvim/plugin/orgmode.lua
@@ -6,7 +6,7 @@ require('orgmode').setup({
   org_agenda_skip_deadline_if_done = true,
   org_deadline_warning_days = 4,
   org_default_notes_file = '~/Documents/sb/inbox.org',
-  org_todo_keywords = { 'TODO(t)', 'ON HOLD(o)', 'BLOCKED(b)', '|', 'DONE(d)', 'DELEGATED(l)'},
+  org_todo_keywords = { 'TODO(t)', 'ON HOLD(o)', 'BLOCKED(b)', 'DELAYED(y)', '|', 'DONE(d)', 'DELEGATED(l)'},
   org_log_into_drawer = 'LOGBOOK',
   org_tags_column = 100,
   org_archive_location = 'archive/%s',
