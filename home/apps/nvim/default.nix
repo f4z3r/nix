e0923d50731373d2dc57cde8f09eaa1b60883a11
Jakob Beckmann <jakobbeckmann@pm.me>
2023-11-22 20:19:37 +0100

    chore: system update

--- home/apps/nvim/default.nix
+++ home/apps/nvim/default.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 
 let
+  # TODO remove neotest once fixed
+  neotest = pkgs.vimUtils.buildVimPlugin {
+    pname = "neotest";
+    version = "2023-11-13";
+    src = pkgs.fetchFromGitHub {
+      owner = "nvim-neotest";
+      repo = "neotest";
+      rev = "d424d262d01bccc1e0b038c9a7220a755afd2a1f";
+      sha256 = "1sg8m77hik1gffrqy4038sivhr8yhg536dp6yr5gbnbrjvc35dgm";
+    };
+    meta.homepage = "https://github.com/nvim-neotest/neotest/";
+  };
+
   gruvbox-material-nvim = pkgs.vimUtils.buildVimPlugin {
     name = "gruvbox-material.nvim";
     src = pkgs.fetchFromGitHub {
