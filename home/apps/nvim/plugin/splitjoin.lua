bd4d63afa18430c4a77b14721a99d5aa1ddea857
Jakob Beckmann <jakobbeckmann@pm.me>
2024-01-31 14:25:26 +0100

    feat: add splitjoin config to handle commas better

--- home/apps/nvim/plugin/splitjoin.lua
+++ home/apps/nvim/plugin/splitjoin.lua
@@ -1,1 +1,16 @@
-require('mini.splitjoin').setup({})
+local splitjoin = require('mini.splitjoin')
+local gen_hook = splitjoin.gen_hook
+
+local brackets = {
+  '%b{}',
+  '%b[]',
+  '%b()',
+}
+
+local add_comma = gen_hook.add_trailing_separator({ brackets = brackets })
+local del_comma = gen_hook.del_trailing_separator({ brackets = brackets })
+
+splitjoin.setup({
+  split = { hooks_post = { add_comma } },
+  join = { hooks_post = { del_comma } },
+})
