f12e1786776bd8365a5994a688b199ce89600724
Jakob Beckmann <jakobbeckmann@pm.me>
2024-01-31 14:47:01 +0100

    feat: improve splitjoin for better padding on join of curly brackets

--- home/apps/nvim/plugin/splitjoin.lua
+++ home/apps/nvim/plugin/splitjoin.lua
@@ -1,16 +1,13 @@
 local splitjoin = require('mini.splitjoin')
 local gen_hook = splitjoin.gen_hook
 
-local brackets = {
-  '%b{}',
-  '%b[]',
-  '%b()',
-}
+local curly = { '%b{}' }
 
-local add_comma = gen_hook.add_trailing_separator({ brackets = brackets })
-local del_comma = gen_hook.del_trailing_separator({ brackets = brackets })
+local add_comma = gen_hook.add_trailing_separator()
+local del_comma = gen_hook.del_trailing_separator()
+local pad_curly = gen_hook.pad_brackets({ brackets = curly })
 
 splitjoin.setup({
   split = { hooks_post = { add_comma } },
-  join = { hooks_post = { del_comma } },
+  join = { hooks_post = { del_comma, pad_curly } },
 })
