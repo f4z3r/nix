7a714b513c9f8b429211558625a430ba55479ee9
Jakob Beckmann <jakobbeckmann@pm.me>
2024-02-03 13:44:40 +0100

    fix: update gruvbox highlights due to treesitter breaking changes

--- home/apps/nvim/plugin/gruvbox.lua
+++ home/apps/nvim/plugin/gruvbox.lua
@@ -1,15 +1,30 @@
 require('gruvbox-material').setup()
 
 vim.api.nvim_set_hl(0, 'CursorLineNr', { ctermfg = 208, bold = true, fg = "#e78a4e" })
-vim.api.nvim_set_hl(0, '@markup.heading.1', { link = "markdownH1", default = true })
-vim.api.nvim_set_hl(0, '@markup.heading.2', { link = "markdownH2", default = true })
-vim.api.nvim_set_hl(0, '@markup.heading.3', { link = "markdownH3", default = true })
-vim.api.nvim_set_hl(0, '@markup.heading.4', { link = "markdownH4", default = true })
-vim.api.nvim_set_hl(0, '@markup.heading.5', { link = "markdownH5", default = true })
-vim.api.nvim_set_hl(0, '@markup.heading.6', { link = "markdownH6", default = true })
-vim.api.nvim_set_hl(0, '@markup.link', { link = "Underlined", default = true })
-vim.api.nvim_set_hl(0, '@markup.list', { link = "Todo", default = true })
-vim.api.nvim_set_hl(0, '@markup.quote', { link = "jsonQuote", default = true })
-vim.api.nvim_set_hl(0, '@markup.raw.inline', { link = "IndentRainbowViolet", default = true })
-vim.api.nvim_set_hl(0, '@markup.bold', { link = "markdownBold", default = true })
-vim.api.nvim_set_hl(0, '@markup.italic', { link = "markdownItalic", default = true })
+
+local mapping = {
+  ['@markup.heading.1'] = { link = "Orange", default = true },
+  ['@markup.heading.2'] = { link = "Green", default = true },
+  ['@markup.heading.3'] = { link = "Aqua", default = true },
+  ['@markup.heading.4'] = { link = "Red", default = true },
+  ['@markup.heading.5'] = { link = "Purple", default = true },
+  ['@markup.heading.6'] = { link = "Yellow", default = true },
+  ['@markup.link'] = { link = "@text.uri", default = true },
+  ['@markup.link.url'] = { link = "@text.uri", default = true },
+  ['@markup.list'] = { link = "Orange", default = true },
+  ['@markup.list.checked'] = { link = "Green", default = true },
+  ['@markup.list.numbered'] = { link = "Aqua", default = true },
+  ['@markup.list.unchecked'] = { link = "Red", default = true },
+  ['@markup.list.unnumbered'] = { link = "Orange", default = true },
+  ['@markup.quote'] = { link = "jsonQuote", default = true },
+  ['@markup.raw'] = { link = "IndentRainbowViolet", default = true },
+  ['@markup.raw.inline'] = { link = "IndentRainbowViolet", default = true },
+  ['@markup.raw.block'] = { link = "IndentRainbowViolet", default = true },
+  ['@markup.bold'] = { link = "markdownBold", default = true },
+  ['@markup.italic'] = { link = "markdownItalic", default = true },
+  ['@punctuation.special'] = { link = "Aqua", default = true },
+}
+
+for key, value in pairs(mapping) do
+  vim.api.nvim_set_hl(0, key, value)
+end
