9fa674342c4fa98f9e302bd4987e2a2e8d02ca86
Jakob Beckmann <jakobbeckmann@pm.me>
2024-02-12 11:37:24 +0100

    feat: make tmux popup be bound to window instead of session

--- home/apps/tmux/scripts/tmux-popup.sh
+++ home/apps/tmux/scripts/tmux-popup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-window="$(tmux display-message -p '#S')"
+# take window id and remove leading @
+window="$(tmux display-message -p '#{window_id}')"
+window="${window#*@}"
+
+# create popup based on window id
 tmux display-popup -d "#{pane_current_path}" -xC -yC -w 80% -h 75% -E "tmux attach-session -t popup-$window || tmux new-session -s popup-$window"
