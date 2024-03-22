b90f947c6b6c0db9b7217ff3739fd71afe9df6bf
Jakob Beckmann <jakobbeckmann@pm.me>
2024-03-22 18:55:45 +0100

    impr: disable treesitter context except for yaml files for performance

--- home/apps/nvim/plugin/treesitter-context.lua
+++ home/apps/nvim/plugin/treesitter-context.lua
@@ -10,5 +10,8 @@ require("treesitter-context").setup({
   -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
   separator = nil,
   zindex = 20, -- The Z-index of the context window
-  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+  on_attach = function(_bufnum)
+    -- only attach on yaml
+    return vim.bo.filetype == "yaml"
+  end,
 })
