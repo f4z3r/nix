f0ccfb160fa3c9cf011b9b5ad770318194c27799
Jakob Beckmann <jakobbeckmann@pm.me>
2024-03-24 18:45:23 +0100

    fix: remove gruvbox extra mappings

--- home/apps/nvim/plugin/gruvbox.lua
+++ home/apps/nvim/plugin/gruvbox.lua
@@ -49,34 +49,3 @@ vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { link = "CmpItemKindMethod" })
 vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#d65d0e", bg = "NONE" })
 vim.api.nvim_set_hl(0, "CmpItemKindColor", { link = "CmpItemKindInterface" })
 vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { link = "CmpItemKindInterface" })
-
-local mapping = {
-  ["@markup.heading.1"] = { link = "Orange", default = true },
-  ["@markup.heading.2"] = { link = "Green", default = true },
-  ["@markup.heading.3"] = { link = "Aqua", default = true },
-  ["@markup.heading.4"] = { link = "Red", default = true },
-  ["@markup.heading.5"] = { link = "Purple", default = true },
-  ["@markup.heading.6"] = { link = "Yellow", default = true },
-  ["@markup.link"] = { link = "@text.uri", default = true },
-  ["@markup.link.url"] = { link = "@text.uri", default = true },
-  ["@markup.list"] = { link = "Orange", default = true },
-  ["@markup.list.checked"] = { link = "Green", default = true },
-  ["@markup.list.numbered"] = { link = "Aqua", default = true },
-  ["@markup.list.unchecked"] = { link = "Red", default = true },
-  ["@markup.list.unnumbered"] = { link = "Orange", default = true },
-  ["@markup.quote"] = { link = "jsonQuote", default = true },
-  ["@markup.raw"] = { link = "IndentRainbowViolet", default = true },
-  ["@markup.raw.inline"] = { link = "IndentRainbowViolet", default = true },
-  ["@markup.raw.block"] = { link = "IndentRainbowViolet", default = true },
-  ["@markup.bold"] = { link = "markdownBold", default = true },
-  ["@markup.italic"] = { link = "markdownItalic", default = true },
-  ["@punctuation.special"] = { link = "Aqua", default = true },
-  ["@comment.todo"] = { link = "Todo", default = true },
-  ["@comment.note"] = { link = "Blue", default = true },
-  ["@comment.warning"] = { link = "Warning", default = true },
-  ["@comment.error"] = { link = "Error", default = true },
-}
-
-for key, value in pairs(mapping) do
-  vim.api.nvim_set_hl(0, key, value)
-end
