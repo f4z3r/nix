061dfc118bb41d78a88c59c08dd6c287e1f62992
Jakob Beckmann <jakobbeckmann@pm.me>
2024-04-11 20:24:32 +0200

    impr(rofi): improve rofi theme

--- home/apps/rofi/default.nix
+++ home/apps/rofi/default.nix
@@ -4,18 +4,21 @@
     cycle = true;
     location = "center";
     terminal = "${pkgs.wezterm}/bin/wezterm";
-    theme = "rounded-gray-dark";
+    theme = "improved";
     extraConfig = {
       kb-cancel = "Control+y";
     };
   };
 
   home.file = {
-    ".local/share/rofi/themes/rounded-common.rasi" = {
-      source = ./themes/rounded-common.rasi;
+    ".local/share/rofi/themes/improved.rasi" = {
+      source = ./themes/improved.rasi;
     };
-    ".local/share/rofi/themes/rounded-gray-dark.rasi" = {
-      source = ./themes/rounded-gray-dark.rasi;
+    ".local/share/rofi/themes/shared/colors.rasi" = {
+      source = ./themes/shared/colors.rasi;
+    };
+    ".local/share/rofi/themes/shared/fonts.rasi" = {
+      source = ./themes/shared/fonts.rasi;
     };
   };
 }
