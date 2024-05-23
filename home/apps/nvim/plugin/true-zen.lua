334117997838047963563fda75aa3ee758ea6820
Jakob Beckmann <f4z3r-github@pm.me>
2024-05-23 09:44:20 +0200

    fix(nvim): improve zen mode to have full width for my coding

--- home/apps/nvim/plugin/true-zen.lua
+++ home/apps/nvim/plugin/true-zen.lua
@@ -1,4 +1,12 @@
 require("true-zen").setup({
+  modes = {
+    ataraxis = {
+      minimum_writing_area = {
+        width = 100,
+        height = 44,
+      },
+    },
+  },
   integrations = {
     lualine = true,
   },
