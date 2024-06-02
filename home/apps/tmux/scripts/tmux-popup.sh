a3cb87bf6b6f94aae987a55f94319c9a2a36e2b0
Jakob Beckmann <f4z3r-github@pm.me>
2024-06-02 13:32:42 +0200

    impr(tmux): only generate a single popup per session

--- home/apps/tmux/scripts/tmux-popup.sh
+++ home/apps/tmux/scripts/tmux-popup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# take window id and remove leading @
-window="$(tmux display-message -p '#{window_id}')"
-window="${window#*@}"
+# take the session name for the popup
+session="$(tmux display-message -p '#{session_name}')"
 
 # create popup based on window id
-tmux display-popup -d "#{pane_current_path}" -xC -yC -w 80% -h 75% -E "tmux attach-session -t popup-$window || tmux new-session -s popup-$window"
+tmux display-popup -d "#{pane_current_path}" -xC -yC -w 80% -h 75% -E "tmux attach-session -t popup-$session || tmux new-session -s popup-$session"
