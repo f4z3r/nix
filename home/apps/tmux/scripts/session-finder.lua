e4aa21e5cd48b9b1ef04a36a7a945b22e1fd5339
Jakob Beckmann <f4z3r-github@pm.me>
2024-06-02 13:23:52 +0200

    fix(tmux): do not create dummy session on rofi abort for tmux-session-finder

--- home/apps/tmux/scripts/session-finder.lua
+++ home/apps/tmux/scripts/session-finder.lua
@@ -1,22 +1,34 @@
 #!/usr/bin/env luajit
 
 local io = require("io")
-local json = require("rapidjson")
+local os = require("os")
 local string = require("string")
 local table = require("table")
 
+local json = require("rapidjson")
+
 local function run(cmd)
-  local ph = assert(io.popen(cmd, "r"))
-  local out = ph:read("*a")
-  ph:close()
-  return out:gsub("\n$", "")
+  local filename = os.tmpname()
+  local command = string.format("%s > %s", cmd, filename)
+  local success, _, code = os.execute(command)
+  local fh = assert(io.open(filename, "r"))
+  local out = fh:read("*a")
+  fh:close()
+  os.remove(filename)
+  if not success then
+    return code, out
+  end
+  return 0, out:gsub("\n$", "")
 end
 
 local function get_repo_paths()
   local cmd = "gfold ~/opt -c never -d json"
-  local out = run(cmd)
+  local code, out = run(cmd)
+  if code ~= 0 then
+    os.exit(code)
+  end
   local repos = json.decode(out)
-  local res = { '/home/f4z3r/notes '}
+  local res = { "/home/f4z3r/notes " }
   for _, repo in ipairs(repos) do
     res[#res + 1] = string.format("%s/%s", repo.parent, repo.name)
   end
@@ -26,7 +38,11 @@ end
 local function pick(options)
   local content = table.concat(options, "\n")
   local cmd = string.format('echo -ne "%s" | rofi -dmenu -p "Repository" -i -no-custom', content)
-  return run(cmd)
+  local code, out = run(cmd)
+  if code ~= 0 then
+    os.exit(code)
+  end
+  return out
 end
 
 local function escape_dir_name(name)
@@ -48,6 +64,9 @@ end
 local function main()
   local repos = get_repo_paths()
   local selected = pick(repos)
+  if not selected or selected == "" then
+    os.exit(1)
+  end
   local session_name = escape_dir_name(selected)
   local exists = tmux_session_exists(session_name)
   if not exists then
