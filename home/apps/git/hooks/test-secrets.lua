7646330e4480ac77274a1c383810bb4487670d47
Jakob Beckmann <f4z3r-github@pm.me>
2024-06-12 11:18:59 +0200

    impr(git): add key headers to pre-commit check

--- home/apps/git/hooks/test-secrets.lua
+++ home/apps/git/hooks/test-secrets.lua
@@ -10,6 +10,18 @@ local secret_patterns = {
   "secret.*=",
 }
 
+local key_file_headers = {
+  "BEGIN RSA PRIVATE KEY",
+  "BEGIN DSA PRIVATE KEY",
+  "BEGIN EC PRIVATE KEY",
+  "BEGIN OPENSSH PRIVATE KEY",
+  "BEGIN PRIVATE KEY",
+  "BEGIN SSH2 ENCRYPTED PRIVATE KEY",
+  "BEGIN PGP PRIVATE KEY BLOCK",
+  "BEGIN ENCRYPTED PRIVATE KEY",
+  "BEGIN OpenVPN Static key V1",
+}
+
 local function get_staged_additions()
   local pid = assert(io.popen("git diff-index -p --cached --no-color HEAD"))
   local out = {}
@@ -22,10 +34,19 @@ local function get_staged_additions()
   return out
 end
 
-local function updates_secret(line)
+local function updates_secret_by_pattern(line, patterns)
   local lower = line:lower()
-  for _, pattern in ipairs(secret_patterns) do
+  for _, pattern in ipairs(patterns) do
     if string.find(lower, pattern) ~= nil then
+      return true
+    end
+  end
+  return false
+end
+
+local function updates_secret_by_fixed(line, substrings)
+  for _, substring in ipairs(substrings) do
+    if string.find(line, substring, 1, true) ~= nil then
       return true
     end
   end
@@ -36,7 +57,7 @@ local function main()
   local added_lines = get_staged_additions()
   local secret_lines = {}
   for _, line in ipairs(added_lines) do
-    if updates_secret(line) then
+    if updates_secret_by_pattern(line, secret_patterns) or updates_secret_by_fixed(line, key_file_headers) then
       secret_lines[#secret_lines + 1] = line
     end
   end
