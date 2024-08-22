969142b4ef0925650686bebd9d4303dfc5176a35
Jakob Beckmann <f4z3r-github@pm.me>
2024-08-22 17:43:22 +0200

    feat(nvim): add tidy to formatting and diagnostics

--- home/apps/nvim/plugin/none-ls.lua
+++ home/apps/nvim/plugin/none-ls.lua
@@ -16,25 +16,19 @@ null_ls.setup({
     null_ls.builtins.diagnostics.tfsec,
     null_ls.builtins.diagnostics.todo_comments,
     null_ls.builtins.diagnostics.trail_space,
+    null_ls.builtins.diagnostics.tidy,
     -- formatting
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.goimports_reviser,
-
     null_ls.builtins.formatting.just,
-
     null_ls.builtins.formatting.cbfmt, --markdown
-
     null_ls.builtins.formatting.stylua,
-
     null_ls.builtins.formatting.alejandra, --nix
-
     null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.shellharden,
-
     null_ls.builtins.formatting.fish_indent,
-
     null_ls.builtins.formatting.yamlfmt,
-
     null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.formatting.tidy,
   },
 })
