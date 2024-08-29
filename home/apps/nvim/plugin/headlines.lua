702bdae110ef8df77b761033f49e54b1dc841f68
Jakob Beckmann <f4z3r-github@pm.me>
2024-08-29 17:03:27 +0200

    feat(nvim): update headlines to not use fat formatting

--- home/apps/nvim/plugin/headlines.lua
+++ home/apps/nvim/plugin/headlines.lua
@@ -2,9 +2,11 @@ require("headlines").setup({
   norg = {
     headline_highlights = {"Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6"},
     bullets = { "", "󰪥", "󰻂", "󰺕", "○" },
+    fat_headlines = false,
   },
   markdown = {
     headline_highlights = {"Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6"},
     bullets = { "", "󰪥", "󰻂", "󰺕", "○" },
+    fat_headlines = false,
   }
 })
