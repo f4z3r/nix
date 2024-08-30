9a51c845fa4d238a99b412f4715568e81cf76ab1
Jakob Beckmann <f4z3r-github@pm.me>
2024-08-30 07:49:54 +0200

    feat(nvim): do not use bullets on markdown

--- home/apps/nvim/plugin/headlines.lua
+++ home/apps/nvim/plugin/headlines.lua
@@ -6,7 +6,7 @@ require("headlines").setup({
   },
   markdown = {
     headline_highlights = {"Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6"},
-    bullets = { "", "󰪥", "󰻂", "󰺕", "○" },
+    bullets = false,
     fat_headlines = false,
   }
 })
