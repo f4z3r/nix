ad4b299dbfa1c1a1a8c49ba6b36bb11cfd7094b3
Jakob Beckmann <f4z3r-github@pm.me>
2024-08-31 13:00:58 +0200

    feat(nvim): enable environment picking for rest.nvim

--- home/apps/nvim/plugin/rest.lua
+++ home/apps/nvim/plugin/rest.lua
@@ -1,6 +1,24 @@
 require("rest-nvim").setup({
-  keybinds = {
-    { "<localleader>rr", "<cmd>Rest run<cr>", "Run request under the cursor" },
-    { "<localleader>rl", "<cmd>Rest run last<cr>", "Re-run latest request" },
-  }
+  ---@type table<string, fun():string> Table of custom dynamic variables
+  custom_dynamic_variables = {
+    -- see base https://github.com/rest-nvim/rest.nvim/blob/472913298be47e56d9b9a65fa3156c08cbc9a772/lua/rest-nvim/context.lua#L33
+  },
+  env_file = ".env",
+  encode_url = true,
+  skip_ssl_verification = false,
+  -- for version 3 migration
+  -- env = {
+  --   enable = true,
+  --   pattern = ".*%.env.*",
+  -- },
+  -- request = {
+  --   skip_ssl_verification = false,
+  --   hooks = {
+  --     encode_url = true,
+  --   }
+  -- }
 })
+
+vim.keymap.set("n", "<localleader>rr", "<cmd>Rest run<cr>", { desc = "Run rest request under the cursor" })
+vim.keymap.set("n", "<localleader>rl", "<cmd>Rest last<cr>", { desc = "Re-run latest rest request" })
+vim.keymap.set("n", "<localleader>re", require('telescope').extensions.rest.select_env, { desc = "Select rest environment" })
--- home/apps/nvim/plugin/telescope.lua
+++ home/apps/nvim/plugin/telescope.lua
@@ -49,4 +49,5 @@ telescope.setup({
 
 telescope.load_extension("notify")
 telescope.load_extension("git_worktree")
+telescope.load_extension("rest")
 -- telescope.load_extension("yank_history")
