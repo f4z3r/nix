b2900ee7c914a917a15f744c68739b1028149e4d
Jakob Beckmann <f4z3r-github@pm.me>
2024-09-01 12:55:28 +0200

    chore: system update

--- flake.nix
+++ flake.nix
@@ -60,7 +60,18 @@
 
           home-manager.nixosModules.home-manager
           {
-            nixpkgs.overlays = [neorg-overlay.overlays.default];
+            nixpkgs.overlays = [
+              neorg-overlay.overlays.default
+              (final: prev: {
+                clamav = prev.clamav.overrideAttrs (old: rec {
+                  version = "1.4.0";
+                  src = pkgs.fetchurl {
+                    url = "https://www.clamav.net/downloads/production/${old.pname}-${version}.tar.gz";
+                    hash = "sha256-1nqymeXKBdrT2imaXqc9YCCTcqW+zX8TuaM8KQM4pOY=";
+                  };
+                });
+              })
+            ];
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
