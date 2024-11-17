17ec7585bf243180ae65d33e05e4ef4bf9dbb0bf
Jakob Beckmann <f4z3r-github@pm.me>
2024-11-17 11:52:28 +0100

    feat(networking): add dummy certificate and hosts entry, block chromecast

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -10,7 +10,9 @@
     networkmanager.enable = true;
 
     hosts = {
-      "127.0.0.1" = ["kiali.apps.example.com" "httpbin.apps.example.com"];
+      "127.0.0.1" = [
+        "example.com"
+      ];
     };
 
     nftables.enable = true;
@@ -21,37 +23,37 @@
       interfaces.wlp0s20f3 = {
         allowedTCPPorts = [8080];
         # needed for chromecast on brave
-        allowedUDPPortRanges = [
-          {
-            from = 32768;
-            to = 60999;
-          }
-        ];
+        # allowedUDPPortRanges = [
+        #   {
+        #     from = 32768;
+        #     to = 60999;
+        #   }
+        # ];
       };
     };
   };
   security.pki.certificates = [
     ''
       -----BEGIN CERTIFICATE-----
-      MIIDiTCCAnGgAwIBAgIUJYu2/7VrNNfTBgZBs17HXlH9edQwDQYJKoZIhvcNAQEL
-      BQAwEzERMA8GA1UEAxMIc3ZjIFJvb3QwHhcNMjQxMDI5MTgwNTQ4WhcNMzQwOTA3
-      MTgwNjE4WjATMREwDwYDVQQDEwhzdmMgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQAD
-      ggEPADCCAQoCggEBAM42fmsVcquiwYDwtSztyD2E+7NmK4yjsQSsGWEHmqLtRU6G
-      B9FAebanV6N8Acmgvu8DCrJiHli/GUpBHOFbNiHhLXhPMBQxn7np6FXBBpBLAwrq
-      p389RdfU/M1Q8HwyP/3D2HB5oXBYLGUSiisByGW8WOyyF/0vaas0H2adXjY4wBRX
-      rc1b5G9WZxmQpii8zzubvTbLeY3Xy6q0Fe8YX0NNsRSBCF7cKHTHmRT7WwDT3ZaR
-      A4fIy5JB0pc235H/Gk51d5loydf8eg+B0PtT4lUAyYtuLn1NoodS6w2/7Kbzx6RL
-      nlANgPHjSflvZsdnCYHFZaFz4RYu8O9uJdeGVnkCAwEAAaOB1DCB0TAOBgNVHQ8B
-      Af8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUmpNLS/J2Ki9nm8CE
-      0VYtd5iWoeMwHwYDVR0jBBgwFoAUmpNLS/J2Ki9nm8CE0VYtd5iWoeMwOwYIKwYB
-      BQUHAQEELzAtMCsGCCsGAQUFBzAChh9odHRwOi8vMTI3LjAuMC4xOjgyMDAvdjEv
-      cGtpL2NhMDEGA1UdHwQqMCgwJqAkoCKGIGh0dHA6Ly8xMjcuMC4wLjE6ODIwMC92
-      MS9wa2kvY3JsMA0GCSqGSIb3DQEBCwUAA4IBAQDFgHS+N4GsZVyuRJDdNkPQu+/u
-      k8eQwFGrhZ7EJTQN+GJi6i3GrHPxAawPvPQXeKnGZguWuuj//HsMPxuspPWSwsNE
-      rmQ4wMl1Q2OnvNItCXkPolpWKmw/EfzHcJEjXAI91FKOVxpFKP/H9T2tLdw8/wCR
-      CQfcMEfmUfWu4k7uK6jPn9f5/NgDuu+BblpVyqphtNyiH+cRTVyYgOoI8nE8Wa/K
-      R2pb95rR1KByxDlklSQz3k8EIcNxgZuJrl5Z8thnE1Je+VycUmE5IdKSs5WJCOXL
-      gdzrDESJfbN2WnIX88qoE4GQGwuFiagIfdZ9R3FMyxvcxwgkloosZeIPJPOf
+      MIIDgTCCAmmgAwIBAgIUXFaVS2QR35qA9QefKsJZdryc9hMwDQYJKoZIhvcNAQEL
+      BQAwEzERMA8GA1UEAxMIc3ZjIFJvb3QwHhcNMjQxMTEzMDc1NTA2WhcNMzQwOTIy
+      MDc1NTM2WjATMREwDwYDVQQDEwhzdmMgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQAD
+      ggEPADCCAQoCggEBAMPeDwF3geYWLr5fbDDaT0R4OsjhLaPWhHuYuW29s7Fs0mu3
+      yH5f8UM2mz9bxxNM/sYzrySN91s+7nNk14Vd8mxV7ZAhlBzufiMiVKwStonadRnb
+      NFLSoHQk71nBqjVLmVrV2elUtGOtwx1E0YcuajYqD7YDqSv+SzZW5jg3WBbxPt0P
+      HlkhRvMZ2NL5ZvqhY+jqtJrfTtrwwSdF1nHPys9zIdl3SJ1b8nvbkLOuIuG3mmL6
+      3OqpaknudZtCKALrQgv40QMGdJU1PJ4zGorMlgRLHzdK1nElEV0vInBVSis0Nelg
+      DrzURp3Q+j2tG85O9FtAmc8GLatSbeir/qdO9u0CAwEAAaOBzDCByTAOBgNVHQ8B
+      Af8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUDFc7wFl9ZQUQ5V0O
+      eMc6Q9dI0f4wHwYDVR0jBBgwFoAUDFc7wFl9ZQUQ5V0OeMc6Q9dI0f4wNwYIKwYB
+      BQUHAQEEKzApMCcGCCsGAQUFBzAChhtodHRwOi8vdmF1bHQ6ODIwMC92MS9wa2kv
+      Y2EwLQYDVR0fBCYwJDAioCCgHoYcaHR0cDovL3ZhdWx0OjgyMDAvdjEvcGtpL2Ny
+      bDANBgkqhkiG9w0BAQsFAAOCAQEAmbRRisKqfTCGcg2enl+xGQk+ganspACNDRRI
+      XTwRd01Km78IZ4mFN+6H0JvzpKJ/ZzbvezRwiLxwYGAhV9O5ZLLECjeL0YBsJszc
+      cqZGfKOO2BW2C2JYQ9x2pShZvOmQ5uAAn0uRvZXseJZOteCV0WVM1psmi7ElJkp3
+      W0H8INIPxZ90sJage5c5/ngFtRT8PfL5ihOsEbJYxSk37CG9EtYCgboUxwnDVkSN
+      V81DXeqT2Ge9YOrgeomZlOZ4+DkG9QpkIJbnzOPCVMNTXnbV8M7mPq4Y86tLK6BB
+      tCnJm3b2HpVpti/qwIUg/OJDYX8yxE3JRiwesFUW9EGt3hFBTA==
       -----END CERTIFICATE-----
     ''
   ];
