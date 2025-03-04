646c05f4c625f41b6750a6f6b06b982aff22363b
Jakob Beckmann <f4z3r-github@pm.me>
2025-03-04 20:37:01 +0100

    fix: remove plymouth because it is too space intensive
    I have only a very little boot section, and the plymouth annimation
    requires a lot of space. This makes it difficult to store many systemd
    boot configurations.

--- configuration.nix
+++ configuration.nix
@@ -38,11 +38,6 @@ in {
       };
       efi = {canTouchEfiVariables = true;};
     };
-    plymouth = {
-      enable = true;
-      themePackages = [ pkgs.adi1090x-plymouth-themes ];
-      theme = "hud_3";
-    };
     tmp = {cleanOnBoot = true;};
   };
 
