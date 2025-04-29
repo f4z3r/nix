00e1a4954f2e72cb3cbfdd0bc56d0eb9e282fe17
Jakob Beckmann <f4z3r-github@pm.me>
2025-04-29 07:01:46 +0200

    feat(hyprland): update vpn script to improve waybar feedback
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- home/apps/hyprland/scripts/vpn.lua
+++ home/apps/hyprland/scripts/vpn.lua
@@ -1,12 +1,23 @@
 #!/usr/bin/env luajit
 
+local io = require("io")
 local os = require("os")
 local string = require("string")
+
+local json = require("rapidjson")
+local request = require("http.request")
+
+local TIMEOUT = 2
 
 local COUNRTY_CODES = {
   "ch",
   "uk",
   "de",
+}
+
+local PROTON_SERVERS = {
+  "datacamp limited",
+  "proton ag",
 }
 
 local function is_active(cc)
@@ -19,12 +30,35 @@ local function is_failed(cc)
   return os.execute(cmd) == 0
 end
 
+local function is_proton_isp(isp)
+  local base = string.lower(isp)
+  for _, v in ipairs(PROTON_SERVERS) do
+    if string.find(base, v, 1, true) ~= nil then
+      return true
+    end
+  end
+  return false
+end
+
+local function using_proton_isp()
+  local nok, stream = request.new_from_uri("http://ip-api.com/json/"):go(TIMEOUT)
+  if nok == nil then
+    return false
+  end
+  local body = stream:get_body_as_string(TIMEOUT)
+  if body == nil then
+    return false
+  end
+  local data = json.decode(body)
+  return is_proton_isp(data.isp)
+end
+
 local function main()
   for _, cc in ipairs(COUNRTY_CODES) do
     if is_active(cc) then
       local output = string.upper(cc)
-      if is_failed(cc) then
-        output = string.format("%%{F#CC241D}%s%%F{F-}", output)
+      if is_failed(cc) or not using_proton_isp() then
+        output = string.format('<span color="#CC241D">%s</span>', output)
       end
       print(output)
       return
