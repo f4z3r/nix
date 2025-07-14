0156e83d578fd0bacce47b1d0838be7d1bb76c8d
Jakob Beckmann <f4z3r-github@pm.me>
2025-07-14 08:50:17 +0200

    feat(nvim): update notification on nvim timer
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- home/apps/nvim/plugin/timerly.lua
+++ home/apps/nvim/plugin/timerly.lua
@@ -1,7 +1,13 @@
-local timerly = require('timerly')
+local timerly = require("timerly")
 timerly.setup({
   position = "bottom-right",
   mapping = function(buf)
     vim.keymap.set("n", "q", timerly.toggle, { buffer = buf })
+  end,
+  on_finish = function()
+    vim.notify("Time's up!", vim.log.levels.ERROR, {
+      title = "Timerly",
+      timeout = 0,
+    })
   end,
 })
