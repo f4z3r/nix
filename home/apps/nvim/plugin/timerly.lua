da5e08d271fadde0bbd398b9632e8941893c3a66
Jakob Beckmann <f4z3r-github@pm.me>
2025-09-26 07:38:23 +0200

    feat(nvim): upate focus time in timerly to 50 minutes
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- home/apps/nvim/plugin/timerly.lua
+++ home/apps/nvim/plugin/timerly.lua
@@ -1,5 +1,6 @@
 local timerly = require("timerly")
 timerly.setup({
+  minutes = { 50, 5 },
   position = "bottom-right",
   mapping = function(buf)
     vim.keymap.set("n", "q", timerly.toggle, { buffer = buf })
