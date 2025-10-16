fd69328bb3fdf652ffeb430764ad7dc95c5b9355
Jakob Beckmann <f4z3r-github@pm.me>
2025-10-16 11:14:17 +0200

    feat(nvim): improve abbreviation for todos and fixes
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- home/apps/nvim/lua/bindings/abbrevs.lua
+++ home/apps/nvim/lua/bindings/abbrevs.lua
@@ -1,8 +1,9 @@
 -- Abbreviations
 
-vim.keymap.set('ia', 'ttodo', 'TODO(@f4z3r):', { noremap = true, silent = true })
-vim.keymap.set('ia', 'ffix', 'FIX(@f4z3r):', { noremap = true, silent = true })
-vim.keymap.set('ia', 'xxx', 'XXX(@f4z3r):', { noremap = true, silent = true })
+vim.keymap.set('ia', 'ttodo', 'TODO: f4z3r -', { noremap = true, silent = true })
+vim.keymap.set('ia', 'ffix', 'FIX: f4z3r -', { noremap = true, silent = true })
+vim.keymap.set('ia', 'xxx', 'XXX: f4z3r -', { noremap = true, silent = true })
+vim.keymap.set('ia', 'tnote', 'NOTE: f4z3r -', { noremap = true, silent = true })
 
 vim.keymap.set('c', 'diff', 'DiffviewOpen', { noremap = true, silent = true })
 vim.keymap.set('c', 'hist', 'DiffviewFileHistory', { noremap = true, silent = true })
