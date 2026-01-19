c0a7ae4d7a3aeb21ed795c7220c3803f1badced0
Jakob Beckmann <f4z3r-github@pm.me>
2026-01-19 17:32:35 +0100

    fix(nvim): increase default foldlevel to 12
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- home/apps/nvim/lua/scrolling.lua
+++ home/apps/nvim/lua/scrolling.lua
@@ -5,7 +5,7 @@ vim.o.sidescrolloff = 5
 
 -- vim.o.foldcolumn = 0
 vim.o.foldenable = true
-vim.o.foldlevel = 8
-vim.o.foldlevelstart = 8
+vim.o.foldlevel = 12
+vim.o.foldlevelstart = 12
 vim.o.foldopen = "all"
 vim.o.foldclose = "all"
