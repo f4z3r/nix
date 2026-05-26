0e81ccdb6b4762950dbf91341d80a13d13acd294
Jakob Beckmann <f4z3r-github@pm.me>
2026-05-26 07:11:46 +0200

    chore(pi): update to version 0.2.2
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- home/apps/nvim/plugin/pi.lua
+++ home/apps/nvim/plugin/pi.lua
@@ -14,7 +14,7 @@ require("pi").setup({
     "/home/f4z3r/notes/resources/ai/:/home/f4z3r/notes/resources/ai:ro",
     "-v",
     vim.fn.getcwd() .. ":/home/f4z3r/workspace",
-    "ghcr.io/f4z3r/pi-agent-image:v0.2.1",
+    "ghcr.io/f4z3r/pi-agent-image:v0.2.2",
     "pi",
   },
   provider = "openrouter",
